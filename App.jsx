78d10ec0b121dafebb586e2e38265e53f10277cc
anamsayyed016 <anamsayyed58@gmail.com>
2025-02-20 02:20:14 +0530

    reactprac

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Nav from './Nav'
+
+function App() {
+ 
+
+  return (
+    <>
+      <Nav/>
+    </>
+  )
+}
+
+export default App
---
+++ Nav.jsx
@@ -0,0 +1,21 @@
+
+function Nav()
+{
+    return(
+        <>
+        <div>
+            <ul className="Nav-nav-bar">
+                <li >Logo</li>
+            
+                
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Gallery</li>
+                    <li>Conatct</li>
+                </ul>
+        </div>
+        </>
+    )
+}
+
+export default Nav
